d56c0618c233c844ef45f42dc24d1e8ace69283c
vybjan <jan.vyborny@stadlerrail.com>
2024-10-19 18:31:02 +0200

    Project structure refactored

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project Euler Solutions by Jan Vyborny
+This package contains my solutions to Project Euler problems.
+
+## What is Project Euler?
+
+[ProjectEuler website](https://projecteuler.net/)
+
+Project Euler is a website offering a collection of challenging mathematical and computer programming problems. These problems require a blend of mathematical insight and programming skills to solve. The platform is designed to encourage learning and exploration in a fun and engaging way, making it suitable for students, professionals, and enthusiasts who enjoy tackling complex problems.
+
+## Why this package?
+
+Initieally I created this package to learn basics of pyhton in engaging and interesting way. Also I enjoyed learning about some iteresting mathematical problems.
---
+++ tools/folder_creator.py
@@ -0,0 +1,44 @@
+"""
+Script to create a series of folders in a specified directory
+"""
+
+from pathlib import Path
+import argparse as ap
+
+
+def main(i_start: int, i_end: int, path: Path) -> None:
+    """
+    Create a series of folders in a specified directory
+
+    :param i_start: start index for folder creation
+    :param i_end: end index for folder creation
+    :param path: path to create folders in
+    :raises ValueError: if start or end values are negative
+    :raises ValueError: if start or end values are greater than 1000
+    :raises ValueError: if start value is greater than end value
+    """
+
+    if any(i < 0 for i in [i_start, i_end]):
+        raise ValueError("Start and end values must be positive integers")
+    elif any(i > 1000 for i in [i_start, i_end]):
+        raise ValueError("Start and end values must be less than 1000")
+    elif i_start > i_end:
+        raise ValueError("Start value must be less than or equal to end value")
+
+    if not path:
+        path = Path.cwd()
+
+    for i in range(i_start, i_end + 1):
+        directory_number = "0" * (3 - len(str(i))) + str(i)
+        directory = path / f"p_{directory_number}"
+        directory.mkdir(exist_ok=True)
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser()
+    parser.add_argument("i_start", type=int, help="Start value for folder creation")
+    parser.add_argument("i_end", type=int, help="End value for folder creation")
+    parser.add_argument("--path", type=Path, help="Path to create folders in")
+
+    args = parser.parse_args()
+    main(args.i_start, args.i_end, args.path)
